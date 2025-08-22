30cb5f6ad6f9dca330e941a2b5ea30316f98c461
Jonatan0234 <48022944+Jonatan0234@users.noreply.github.com>
2025-08-22 17:59:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const exercisesData = {
     "pecho": [
         { 
             name: "Press de Banca", 
-            img src="Pictures/1InclinedDBbenchpress.gif" alt="press de banca",
+            gif: "https://www.fitnessrxformen.com/wp-content/uploads/2016/09/Dumbbell-Incline-Press.gif",
             description: "Acostado en un banco, baja la barra al pecho y luego empuja hacia arriba. Mantén la espalda apoyada y los pies firmes en el suelo."
         },
         { 
